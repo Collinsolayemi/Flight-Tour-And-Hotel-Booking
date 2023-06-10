9a6141e0dbc53b9dfa7b2e01a8b4ea11e29b3db5
Collinsolayemi <collinsolayemi@gmail.com>
2023-06-10 04:35:59 -0700

    user entity

--- src/user/entities/user.entity.ts
+++ src/user/entities/user.entity.ts
@@ -1,1 +1,26 @@
-export class User {}
+import { Entity, Column, PrimaryGeneratedColumn } from 'typeorm';
+//import { Exclude } from 'class-transformer';
+
+@Entity()
+export class User {
+  @PrimaryGeneratedColumn()
+  id: number;
+
+  @Column()
+  username: string;
+
+  @Column()
+  password: string;
+
+  @Column()
+  email: string;
+
+  @Column()
+  phone_number: string;
+
+  @Column()
+  profile_picture: string;
+
+  @Column()
+  birthday: string;
+}
