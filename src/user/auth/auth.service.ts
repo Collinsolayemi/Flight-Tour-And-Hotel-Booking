f8159fb63cd1f64c3d85600da2370e1b315bdfee
Collinsolayemi <collinsolayemi@gmail.com>
2023-07-04 17:54:26 -0700

    ch

--- src/user/auth/auth.service.ts
+++ src/user/auth/auth.service.ts
@@ -1,5 +1,5 @@
 import {
-  Injectable,
+ Injectable,
   ConflictException,
   BadRequestException,
   NotFoundException,
--- src/user/auth/email/mail-service.ts
+++ src/user/auth/email/mail-service.ts
@@ -1,4 +1,4 @@
-import { Injectable } from '@nestjs/common';
+ import { Injectable } from '@nestjs/common';
 import * as nodemailer from 'nodemailer';
 import * as sendGridTransport from 'nodemailer-sendgrid-transport';
 
@@ -8,16 +8,29 @@ export class MailService {
 
   constructor() {
     // Create a nodemailer transporter
-   
+    // this.transporter = nodemailer.createTransport({
+    //   host: 'smtp.sendgrid.net',
+
+    //   auth: {
+    //     user: 'apikey',
+    //     pass: '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',
+    //     pass: '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',
+    //   },
+    //   secure: true, // Enable secure connection (SSL/TLS)
+    // });
+
 
     
+
+    
+   
   }
 
   async send(options: {
     to: string;
     subject: string;
     text: string;
-  }): Promise<boolean> {
+  }): Promise < boolean > {
     try {
       // Compose the email
       const mailOptions = {
@@ -33,9 +46,10 @@ export class MailService {
       console.log('Email sent:', info.messageId);
 
       return true;
-    } catch (error) {
+    } catch(error) {
       console.error('Error sending email:', error);
       return false;
     }
   }
-}
+  }
+
