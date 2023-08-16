23553e3e77fa0b34880ac266643a27821b79ff69
Collinsolayemi <collinsolayemi@gmail.com>
2023-08-16 15:36:33 -0700

    code restructure

--- src/cloudinary/constant.js
+++
@@ -1,4 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.CLOUDINARY = void 0;
-exports.CLOUDINARY = 'Cloudinary';
--- src/user/auth/dto/update-user.dto.js
+++
@@ -1,4 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-//import { CreateUserDto } from './create-user.dto';
-//export class UpdateUserDto extends PartialType(CreateUserDto) {}
